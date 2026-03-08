1a7e20a108d4323793c7a373815d0fba1ed4090a
devprime90-creator <devprime90@gmail.com>
2026-03-08 13:42:07 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ const serviceLinks = [
 
 const navLinks = [
   { name: "Home", href: "/" },
-  { name: "About", href: "#about" },
+  { name: "About", href: "/about" },
   { name: "Gallery", href: "/gallery" },
   { name: "Contact", href: "/contact" },
 ]
